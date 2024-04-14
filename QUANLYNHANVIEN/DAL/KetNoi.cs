5cdaa184b3bb4fcf9af3918547d87760fa636b3a
Phạm Duy Phúc Nguyên <94799139+nguyen743@users.noreply.github.com>
2024-04-14 16:42:52 +0700

    'update'

---
+++ QUANLYNHANVIEN/DAL/KetNoi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    public class KetNoi
+    {
+        public SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-4S06LH6\SQLEXPRESS;Initial Catalog=QUANLYNHANVIEN;Integrated Security=True");
+        public void CheckConnection()
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+    }
+}
